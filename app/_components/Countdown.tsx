b8573df9caa6be628cb56a0d50bf2217abc91e24
Florian Winkler <florian.winkler@muse-case.com>
2026-03-26 09:41:21 +0100

    chore: add blue background

---
+++ app/_components/Countdown.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+interface Props {
+  targetIso: string;
+}
+
+const Countdown: FC<Props> = ({ targetIso }) => {
+  return <div>Festival Countdown</div>;
+};
+
+export default Countdown;

e7557ebadcddcc3e13ea67a4cac3660ace359ea8
Florian Winkler <florian.winkler@muse-case.com>
2026-03-26 16:01:09 +0100

    chore: add meta data for draufsicht festival

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Draufsicht Festival",
-  description: "",
+  title: "Draufsicht Festival - Karlshöhe Stuttgart",
+  description:
+    "Zwei Tage, zwei Bühnen, unzählige besondere Momente: Das Draufsicht Festival verwandelte die Karlshöhe in Stuttgart in einen Ort voller Kunst, Kultur und Mitmachaktionen.",
 };
 
 export default function RootLayout({
